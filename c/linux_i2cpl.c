4966d78d2b6bf3501949f95df7b64cda936d79a6
Roy Ratcliffe <roy@ratcliffe.me>
2024-11-22 16:06:53 +0000

    refactor: i2c_funcs_stream_to_int_2

--- c/linux_i2cpl.c
+++ c/linux_i2cpl.c
@@ -29,18 +29,18 @@ foreign_t i2c_slave_2(term_t Stream, term_t Address)
  * long with an unsigned 64-bit integer. It relies on C to coerce the former to
  * the latter.
  */
-foreign_t i2c_funcs_2(term_t Stream, term_t Funcs)
+foreign_t i2c_funcs_stream_to_int_2(term_t Stream, term_t Int)
 { IOSTREAM *stream;
+  if (!PL_get_stream(Stream, &stream, 0)) PL_fail;
   unsigned long funcs;
-  if (!PL_get_stream(Stream, &stream, SIO_OUTPUT)) PL_fail;
   if (0 > ioctl(Sfileno(stream), I2C_FUNCS, &funcs)) PL_fail;
-  if (!PL_unify_uint64(Funcs, funcs)) PL_fail;
+  if (!PL_unify_uint64(Int, funcs)) PL_fail;
   PL_succeed;
 }
 
 install_t install_linux_i2c()
 { PL_register_foreign("i2c_slave", 2, i2c_slave_2, 0);
-  PL_register_foreign("i2c_funcs", 2, i2c_funcs_2, 0);
+  PL_register_foreign("i2c_funcs_stream_to_int", 2, i2c_funcs_stream_to_int_2, 0);
 }
 
 install_t uninstall_linux_i2c()
