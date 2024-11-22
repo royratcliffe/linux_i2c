fc49b28acc593c8d7449e963317db7da68e3cb16
Roy Ratcliffe <roy@ratcliffe.me>
2024-11-22 19:20:00 +0000

    feat: i2c_read, write

--- prolog/linux/i2c.pl
+++ prolog/linux/i2c.pl
@@ -5,8 +5,14 @@
 */
 
 :- module(linux_i2c,
-          [ i2c_funcs/2                         % +Stream,-Funcs
+          [ i2c_funcs/2,                        % +Stream,-Funcs
+            i2c_open/2,                         % +Pathname,-Dev
+            i2c_write/3,                        % +Dev,++Bytes,-Actual
+            i2c_write/2,                        % +Dev,++Bytes
+            i2c_read/3,                         % +Dev,+Expected,-Bytes
+            i2c_read/2                          % +Dev,+Bytes
           ]).
+:- use_module(library(shlib)).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -30,5 +36,23 @@ details elided.
 %!  i2c_funcs(+Stream, -Funcs) is det.
 
 i2c_funcs(Stream, Funcs) :-
-    i2c_funcs_stream_to_int(Stream, Int),
+    i2c_funcs_dev_to_int(Stream, Int),
     i2c_funcs_int_to_list(Int, Funcs).
+
+%!  i2c_open(+Pathname, -Dev) is semidet.
+
+%!  i2c_write(+Dev, ++Bytes, -Actual) is semidet.
+%!  i2c_write(+Dev, ++Bytes) is semidet.
+%
+%   The 2-arity form fails if the Actual number of bytes written does
+%   not match the number of Bytes.
+
+i2c_write(Dev, Bytes) :-
+    i2c_write(Dev, Bytes, Actual),
+    length(Bytes, Actual).
+
+%!  i2c_read(+Dev, +Bytes) is semidet.
+
+i2c_read(Dev, Bytes) :-
+    length(Bytes, Expected),
+    i2c_read(Dev, Expected, Bytes).
