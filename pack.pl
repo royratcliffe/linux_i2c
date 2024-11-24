addfb9931e128fbbf621343e192c2d3c6c9baa40
Roy Ratcliffe <roy@ratcliffe.me>
2024-11-24 18:11:32 +0000

    ci: pack title, author, home

--- pack.pl
+++ pack.pl
@@ -1,2 +1,10 @@
 name(linux_i2c).
 version('0.1.0').
+title('I2C Device on Linux').
+
+author('Roy Ratcliffe', 'roy@ratcliffe.me').
+packager('Roy Ratcliffe', 'roy@ratcliffe.me').
+maintainer('Roy Ratcliffe', 'roy@ratcliffe.me').
+
+home('https://github.com/royratcliffe/linux_i2c').
+download('https://github.com/royratcliffe/linux_i2c/releases/*.zip').
